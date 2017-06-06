b0b50c5ae84dba2800a2ad9db7f39dde01b4a036
Emma <doodybrainz@gmail.com>
2017-06-06 14:14:39 -0400

    Add create and build tasks

--- gulpfile.js
+++ gulpfile.js
@@ -2,12 +2,30 @@ var gulp = require('gulp');
 var template = require('gulp-template');
 var data = require('gulp-data');
 var fs = require('fs');
+var createFile = require('create-file');
 
-gulp.task('default', () =>
-    gulp.src('src/index.html')
-    .pipe(data(function(file) {
-      return JSON.parse(fs.readFileSync('./data/index.json'));
-    }))
-    .pipe(template())
-    .pipe(gulp.dest('build'))
+var linkObject = {
+  "linkOne": {
+   "title": "",
+   "url": ""
+ },
+ "linkTwo": {
+  "title": "",
+  "url": ""
+ }
+};
+
+gulp.task('create', () =>
+  createFile('data/index.json', JSON.stringify(linkObject), function (err) {
+    console.log(err);
+  })
+);
+
+gulp.task('build', () =>
+  gulp.src('src/index.html')
+  .pipe(data(function(file) {
+    return JSON.parse(fs.readFileSync('./data/index.json'));
+  }))
+  .pipe(template())
+  .pipe(gulp.dest('build'))
 );
