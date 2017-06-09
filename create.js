655a870d025367852094b6efbf9346124ebd0d29
Emma <doodybrainz@gmail.com>
2017-06-09 15:20:47 -0400

    Add inquirer

--- create.js
+++ create.js
@@ -4,36 +4,56 @@ var createHTML = require('create-html');
 'use strict';
 var inquirer = require('inquirer');
 
+// make number of link questions dynamic
+// make html template dynamic
+
+var linkQuestions = [
+  {
+    type: 'input',
+    name: 'linkOneTitle',
+    message: 'What is the name of your first link?'
+  },
+  {
+    type: 'input',
+    name: 'linkOneUrl',
+    message: 'What is the URL for your first link?'
+  },
+  {
+    type: 'input',
+    name: 'linkTwoTitle',
+    message: 'What is the name of your second link?'
+  },
+  {
+    type: 'input',
+    name: 'linkTwoUrl',
+    message: 'What is the URL for your second link?'
+  },
+  {
+    type: 'input',
+    name: 'linkThreeTitle',
+    message: 'What is the name of your third link?'
+  },
+  {
+    type: 'input',
+    name: 'linkThreeUrl',
+    message: 'What is the URL for your third link?'
+  }
+];
+
 var html = createHTML({
   title: 'site',
-  body: '<a id="one" href="<%= linkOne.url %>"><%= linkOne.title %></a><a href="<%= linkTwo.url %>"><%= linkTwo.title %></a><a href="<%= linkThree.url %>"><%= linkThree.title %></a>',
+  body: '<a id="one" href="<%= linkOneUrl %>"><%= linkOneTitle %></a><a href="<%= linkTwoUrl %>"><%= linkTwoTitle %></a><a href="<%= linkThreeUrl %>"><%= linkThreeTitle %></a>',
   css: './style.css',
   script: 'http://cdnjs.cloudflare.com/ajax/libs/gsap/1.19.0/TweenMax.min.js'
 })
 
-var linkObject = {
-  "linkOne": {
-   "title": "",
-   "url": ""
-   },
-   "linkTwo": {
-    "title": "",
-    "url": ""
-  },
-  "linkThree": {
-   "title": "",
-   "url": ""
-  }
-};
-
 var dir = './src';
 
 var boringCss = 'html {box-sizing: border-box;}*, *:before, *:after {box-sizing: inherit;}body {border: 10px solid gray;height:100vh;padding: 0;margin:0;} a{text-align:center;text-transform:uppercase;letter-spacing:.09em;display:block;margin:0 auto;text-align:center;border:1px solid gray;width:100px;}';
-var excitingCss = 'html {box-sizing: border-box;}*, *:before, *:after {box-sizing: inherit;}body {border: 10px solid #9370DB;height:100vh;padding: 0;margin:0;}a{color:#40E0D0;font-weight:800;font-size:1.2em;text-align:center;text-transform:uppercase;letter-spacing:.09em;position:absolute;display:block;border:1px solid hotpink;width:100px;}a:first-child{top:23%;left:85%;}a:nth-child(2){bottom:42%;right:13%;}a:nth-child(3){top:38%;right:48%;}';
+var litCss = 'html {box-sizing: border-box;}*, *:before, *:after {box-sizing: inherit;}body {border: 10px solid #9370DB;height:100vh;padding: 0;margin:0;}a{color:#40E0D0;font-weight:800;font-size:1.2em;text-align:center;text-transform:uppercase;letter-spacing:.09em;position:absolute;display:block;border:1px solid hotpink;width:100px;}a:first-child{top:23%;left:85%;}a:nth-child(2){bottom:42%;right:13%;}a:nth-child(3){top:38%;right:48%;}';
 
-
-function createFile(style) {
-  fs.writeFile('links.json', JSON.stringify(linkObject, null, 4), (err)  => {
+function createFiles(style, links) {
+  fs.writeFile('links.json', JSON.stringify(links, null, 4), (err)  => {
     if (err) throw err;
   });
   fs.writeFile('index.html', html, function (err) {
@@ -46,17 +66,23 @@ function createFile(style) {
   if (style.which === 'i want my website to be boring.') {
     fs.writeFile('src/style.css', boringCss, function (err) {
       if (err) console.log(err)
-      say.speak('this website is gonna be boring.', 'Alex', .5);
+      // say.speak('this website is gonna be boring.', 'Alex', .5);
     })
   } else {
-    fs.writeFile('src/style.css', excitingCss, function (err) {
+    fs.writeFile('src/style.css', litCss, function (err) {
       if (err) console.log(err)
-      say.speak('this website is gonna be lit!', 'Victoria', 1);
+      // say.speak('this website is gonna be lit!', 'Victoria', 1);
     })
   }
 }
 
-function chooseSiteStyle() {
+function makeLinks() {
+  inquirer.prompt(linkQuestions).then(function (answers) {
+     makeWebsite(answers);
+   });
+}
+
+function makeWebsite(links) {
   inquirer.prompt([
     {
       type: 'list',
@@ -68,8 +94,8 @@ function chooseSiteStyle() {
       }
     }
   ]).then(function (answers) {
-    createFile(answers);
+    createFiles(answers, links);
   });
 }
 
-module.exports.chooseSiteStyle = chooseSiteStyle;
+module.exports.makeLinks = makeLinks;
